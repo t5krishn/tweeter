8c931d7de13115774faba649dd1b6f0e65df2c7d
t5krishn <tk95@live.ca>
2019-07-18 17:33:25 +0000

    Linted and commented new-tweet.js

--- public/scripts/new-tweet.js
+++ public/scripts/new-tweet.js
@@ -1,13 +1,18 @@
+//  new-tweet.js
+//    - This function handles the onClick handler that shows or hides the
+//    new tweet area, with textarea and submit button. This toggle is at the top 
+//    right of the nav bar
 
+// Handles the click and slides the new-tweet area down or up
 const newTweetClickHandler = function() {
-  $('#nav-new-tweet-container').on('click', function(event) {
+  $('#nav-new-tweet-container').on('click', function() {
     $('.new-tweet').animate({
       height: "toggle",
     }, 300, function() {
       $('#tweet-text-area').focus();
     });
   });
-}
+};
 
 
 $(document).ready(function() {
