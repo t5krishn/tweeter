2d44a07d072e3b85c2dd90088c72a7129a832b97
t5krishn <tk95@live.ca>
2019-07-18 17:20:07 +0000

    Linted composer.js

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -7,27 +7,26 @@ const initScrollElements = function() {
   const topOfOthDiv = $("#main-header").offset().top + 280;
   console.log(topOfOthDiv);
   $(window).scroll(function() {
-      if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-        $scrollDown.show(); //reached the desired point -- show div
-        $newTweetBtn.hide();
-      }
-      else{
-        $scrollDown.hide(); //else above the desired point -- hide div
-        $newTweetBtn.show();
-      }
+    if ($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
+      $scrollDown.show(); //reached the desired point -- show div
+      $newTweetBtn.hide();
+    } else {
+      $scrollDown.hide(); //else above the desired point -- hide div
+      $newTweetBtn.show();
+    }
   });
-}
+};
 
 const handleScrollDown = function() {
-  $("#scroll-down").on('click', function(event){
-    $('html, body').animate({scrollTop: 0}, 400);
+  $("#scroll-down").on('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 400);
     $('.new-tweet').show();
     $('#tweet-text-area').focus();
-  })
-}
+  });
+};
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   initScrollElements();
   handleScrollDown();
-})  
+});
